cbe6842991c88568e84672e6f29c9ab38b979b54
simon <simon@journeyman.cc>
2014-03-26 21:55:30 +0000

    Added a basic Luminus webapp template; not really fleshed that out yet.

--- project.clj
+++ project.clj
@@ -1,6 +1,29 @@
 (defproject vending "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [lib-noir "0.8.1"]
+                 [compojure "1.1.6"]
+                 [ring-server "0.3.1"]
+                 [selmer "0.6.4"]
+                 [com.taoensso/timbre "3.1.6"]
+                 [com.taoensso/tower "2.0.2"]
+                 [markdown-clj "0.9.41"]
+                 [environ "0.4.0"]
+                 [enlive "1.1.5"]]
+
+  :repl-options {:init-ns vending.repl}
+  :plugins [[lein-ring "0.8.10"]
+            [lein-environ "0.4.0"]]
+  :ring {:handler vending.handler/app
+         :init    vending.handler/init
+         :destroy vending.handler/destroy}
+  :profiles
+  {:uberjar {:aot :all}
+   :production {:ring {:open-browser? false
+                       :stacktraces?  false
+                       :auto-reload?  false}}
+   :dev {:dependencies [[ring-mock "0.1.5"]
+                        [ring/ring-devel "1.2.2"]]
+         :env {:dev true}}}
+  :min-lein-version "2.0.0")
---
+++ src/vending/routes/home.clj
@@ -0,0 +1,15 @@
+(ns vending.routes.home
+  (:use compojure.core)
+  (:require [vending.views.layout :as layout]
+            [vending.util :as util]))
+
+(defn home-page []
+  (layout/render
+    "home.html" {:content (util/md->html "/md/docs.md")}))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/about" [] (about-page)))

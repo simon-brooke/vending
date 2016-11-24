f8fe873bd426ffae935214627b78e0a1f03b4ee0
simon <simon@journeyman.cc>
2016-11-24 16:05:59 +0000

    OK, this now works, more or less. The REST API is not yet used, and the on-screen representation of the machine isn't nearly as pretty as I intend.

--- src/vending/routes/home.clj
+++ src/vending/routes/home.clj
@@ -1,15 +1,53 @@
 (ns vending.routes.home
   (:use compojure.core)
   (:require [vending.views.layout :as layout]
+            [noir.session :as session]
+            [vending.core :as machine]
             [vending.util :as util]))
 
-(defn home-page []
+
+(def button-actions
+  {"Coin return" machine/coin-return
+   "Add Merk" machine/add-merk
+   "Add Bawbee" machine/add-bawbee
+   "Add Plack" machine/add-plack
+   "Add Bodle" machine/add-bodle
+   "Request Caramel Wafer" machine/get-caramel-wafer
+   "Request Teacake" machine/get-teacake
+   "Request Snowball" machine/get-snowball
+   })
+
+
+(defn- perform-action!
+  "Apply this function to the machine in the session, if there is one, or else to a new default
+  machine; cache the result in the session; and return a rendered page representation of the
+  state of the machine."
+  [function]
+  (let [machine (apply function (list (or (session/get :machine) (machine/make-default-machine))))]
+    (session/put! :machine machine)
+    (layout/render
+      "home.html" {:content (util/md->html "/md/docs.md")
+                   :machine machine
+                   :buttons (keys button-actions)})))
+
+
+(defn home-page
+  "Render the home page with the default machine; in so doing, reset the machine in the session."
+  []
+  (let [machine (machine/make-default-machine)]
+    (session/put! :machine machine)
   (layout/render
-    "home.html" {:content (util/md->html "/md/docs.md")}))
+    "home.html" {:content (util/md->html "/md/docs.md")
+                 :machine machine
+                 :buttons (keys button-actions)})))
+
 
 (defn about-page []
   (layout/render "about.html"))
 
+
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/about" [] (about-page))
+  (POST "/update" [action] (perform-action! (button-actions action)))
+  )
--- src/vending/routes/json.clj
+++ src/vending/routes/json.clj
@@ -8,75 +8,23 @@
             [vending.util :as util]))
 
 
-(defn- perform-action
+(defn- perform-action!
   "Apply this function to the machine in the session, if there is one, or else to a new default
-  machine; cache the result in the session; and return a JSON formatted representation of the result."
+  machine; cache the result in the session; and return a JSON formatted representation of the state
+  of the machine."
   [function]
   (let [machine (apply function (list (or (session/get :machine) (machine/make-default-machine))))]
     (session/put! :machine machine)
     (json/write-str machine)))
 
 
-;;; Each of these action functions perform an action on the machine in the session, if there is one,
-;;; or on a new default machine if there is no machine in the session. They return (and cache in the
-;;; session) the new state of the machine after the action; the machine returned is returned as a
-;;; JSON string.
-
-(defn coin-return-action
-  "Return all the coins that have been tendered since the last sale."
-  []
-  (perform-action machine/coin-return))
-
-
-(defn add-merk-action
-  "Insert one merk into the coin slot of the machine in the session."
-  []
-  (perform-action machine/add-merk))
-
-
-(defn add-bawbee-action
-  "Insert one bawbee into the coin slot of the machine in the session."
-  []
-  (perform-action machine/add-bawbee))
-
-
-(defn add-plack-action
-  "Insert one plack into the coin slot of the machine in the session."
-  []
-  (perform-action machine/add-plack))
-
-
-(defn add-bodle-action
-  "Insert one bodle into the coin slot of the machine in the session."
-  []
-  (perform-action machine/add-bodle))
-
-
-(defn select-caramel-wafer-action
-  "Request one caramel wafer from the machine in the session."
-  []
-  (perform-action machine/get-caramel-wafer))
-
-
-(defn select-teacake-action
-  "Request one teacake from the machine in the session."
-  []
-  (perform-action machine/get-teacake))
-
-
-(defn select-snowball-action
-  "Request one snowball from the machine in the session."
-  []
-  (perform-action machine/get-snowball))
-
-
 (defroutes json-routes
-  (GET "/coin-return" [] (coin-return-action))
-  (GET "/add-merk" [] (add-merk-action))
-  (GET "/add-bawbee" [] (add-bawbee-action))
-  (GET "/add-plack" [] (add-plack-action))
-  (GET "/add-bodle" [] (add-bodle-action))
-  (GET "/select-caramel-wafer" [] (select-caramel-wafer-action))
-  (GET "/select-teacake" [] (select-teacake-action))
-  (GET "/select-snowball" [] (select-snowball-action))
+  (GET "/json/coin-return" [] (perform-action! machine/coin-return))
+  (GET "/json/add-merk" [] (perform-action! machine/add-merk))
+  (GET "/json/add-bawbee" [] (perform-action! machine/add-bawbee))
+  (GET "/json/add-plack" [] (perform-action! machine/add-plack))
+  (GET "/json/add-bodle" [] (perform-action! machine/add-bodle))
+  (GET "/json/select-caramel-wafer" [] (perform-action! machine/get-caramel-wafer))
+  (GET "/json/select-teacake" [] (perform-action! machine/get-teacake))
+  (GET "/json/select-snowball" [] (perform-action! machine/get-snowball))
   )
